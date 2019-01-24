54674e633b0274a48a264deee0d6cef11d0e67cc
danny <dannydorstijn1997@gmail.com>
2019-01-24 11:05:47 +0100

    Cross-platform path creation that is properly null terminated

--- src/database.cpp
+++ src/database.cpp
@@ -36,9 +36,17 @@ make_path(const char* str1, const char* str2)
 
     size_t size;
 
-    size = strlen(str1) + strlen(str2) + 1;
+#ifdef _WIN32
+    char delimiter = '\\';
+#else
+    char delimiter = '/';
+#endif
+
+    // Added 2 extra charachters. One for the "/" and one spare for the null
+    // termination
+    size = strlen(str1) + strlen(str2) + 2;
     char* ret = (char*)malloc(size);
-    snprintf(ret, size, "%s/%s", str1, str2);
+    snprintf(ret, size, "%s%c%s", str1, delimiter, str2);
 
     return ret;
 }
